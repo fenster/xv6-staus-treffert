abda545c3512d8e3197565e2b04d45d804ce6a84
Garret Staus <suspectfenster@gmail.com>
2010-02-18 20:39:13 -0600

    Added code to thread_create.  Might work?

--- thread.h
+++ thread.h
@@ -1,4 +1,20 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "mmu.h"
+#include "umalloc.c"
+
 int thread_create(void*(*start_routine)(void *), void *arg){
+	
+	//Allocate stack using process heap
+	void * stack = malloc(KSTACKSIZE);
+
+	//create new thread
+	if(fork_thread(stack) == 0){
+		//if we are in the child, call to routine
+		start_routine(arg);				
+	}
+	
 	return 0;
 }
 
